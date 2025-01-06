237284464b73d679876e2d8b79c986226577d56b
josh <1071537404@qq.com>
2025-01-07 01:27:53 +0800

    优化稳定性

--- ROS2/ARX_VR.sh
+++ ROS2/ARX_VR.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 source ~/.bashrc
-gnome-terminal -t "VR2SDK" -x bash -c "source install/setup.sh;ros2 run ros_tcp_endpoint default_server_endpoint --ros-args -p ROS_IP:=192.168.31.137 -p ROS_TCP_PORT:=10000;exec bash;"
 
-sleep 1
+workspace=$(pwd)
 
-gnome-terminal -t "arx5_pos_cmd" -x bash -c "source install/setup.sh;ros2 topic echo /ARX_VR_L;exec bash;"
+gnome-terminal -t "unity_tcp" -x bash -c "cd ${workspace};source install/setup.bash;ros2 run serial_port serial_port_node;exec bash;"
 
-gnome-terminal -t "arx5_pos_cmd" -x bash -c "source install/setup.sh;ros2 topic echo /ARX_VR_R;exec bash;"
+sleep 4
 
-gnome-terminal -t "arx5_pos_cmd" -x bash -c "source install/setup.sh;ros2 topic hz /ARX_VR_L;exec bash;"
+gnome-terminal -t "arx5_pos_cmd" -x bash -c "cd ${workspace};source install/setup.bash;ros2 topic echo /ARX_VR_L;exec bash;"
 
-gnome-terminal -t "arx5_pos_cmd" -x bash -c "source install/setup.sh;ros2 topic hz /ARX_VR_R;exec bash;"
+gnome-terminal -t "arx5_pos_cmd" -x bash -c "cd ${workspace};source install/setup.bash;ros2 topic echo /ARX_VR_R;exec bash;"
 
+gnome-terminal -t "arx5_pos_cmd" -x bash -c "cd ${workspace};source install/setup.bash;ros2 topic hz /ARX_VR_L;exec bash;"
+
+gnome-terminal -t "arx5_pos_cmd" -x bash -c "cd ${workspace};source install/setup.bash;ros2 topic hz /ARX_VR_R;exec bash;"
 
--- ROS2/src/ROS-TCP-Endpoint/ros_tcp_endpoint/setup.py
+++
@@ -1,6 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-
-setup(ext_modules=cythonize(["tcp_sender.py",
-                             "communication.py", "service.py", "thread_pauser.py",
-                             "publisher.py", "unity_service.py", "exceptions.py", "subscriber.py"]))
